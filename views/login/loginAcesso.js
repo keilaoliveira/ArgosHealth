9eebe2bcc2b46326d1ec7004fccf05a12770e09f
Keila Oliveira <keila.oliver08@gmail.com>
2019-04-29 18:34:49 -0300

    Iniciando a validação de Login

--- views/login/loginAcesso.js
+++ views/login/loginAcesso.js
@@ -1,16 +1,17 @@
+
 function Login() {
     var done = 0;
     var usuario = document.getElementsByName('txtLogin')[0].value;
     usuario = usuario.toLowerCase();
     var senha = document.getElementsByName('txtSenha')[0].value;
     senha = senha.toLowerCase();
-  
+
     if (usuario == "abc" && senha == "123") {
-      window.location.href = "/home";
-      done = 1;
+        res.redirect('/home');
+        done = 1;
     }
     if (done == 0) {
-      alert("Dados incorretos, tente novamente!");
-  
+        alert("Dados incorretos, tente novamente!");
+
     }
 }
